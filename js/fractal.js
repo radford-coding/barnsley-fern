a058279bc08df8ee2fd1bf4374a275978c194ad5
Matt Radford <quinquex@gmail.com>
2025-01-28 08:03:24 -0800

    baby steps

--- js/fractal.js
+++ js/fractal.js
@@ -1,5 +1,30 @@
-const c = document.getElementById("myCanvas");
-let ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+// https://jsfiddle.net/p49HD/1/
+// https://www.w3schools.com/html/html5_canvas.asp
+// https://en.wikipedia.org/wiki/Barnsley_fern#Construction
+
+
+let canvas, context, width, height, currentPoint;
+
+
+
+const drawPoint = function(point) {
+    s.fillRect(point.x+0.5, point.y+0.5, 2, 2);
+};
+
+const makePoint = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+const initializeCanvas = function() {
+    canvas = document.getElementById("Serpinski");
+    context = canvas.getContext("2d");
+    currentPoint = makePoint(0, 0);
+    width = window.innerWidth;
+    height = window.innerHeight;
+    // console.log(width, height);
+    canvas.width = width;
+    canvas.height = height;
+};
+
+initializeCanvas();
